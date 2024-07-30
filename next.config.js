2fc6a13a087d48046ec798dc766f40865a30f25a
Mofi Rahman <mofi@google.com>
2024-07-30 13:01:12 -0400

    add google auth

--- next.config.js
+++ next.config.js
@@ -6,3 +6,27 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/*
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NbTeEDpPFpbNrUMzcEzpeYtwgQ4auUE",
+  authDomain: "mofilabs-genai-02.firebaseapp.com",
+  projectId: "mofilabs-genai-02",
+  storageBucket: "mofilabs-genai-02.appspot.com",
+  messagingSenderId: "28777015733",
+  appId: "1:28777015733:web:b65f79cb4762606b3991fc",
+  measurementId: "G-3WX175WWXL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+*/
